22ea0d6d661bc3e74929515a3728abd72fadec04
Luiz Henrique Silvério de Souza <02239263610@campus.machado.mch>
2022-11-03 09:52:53 -0300

    Commit inicial

---
+++ SistemaCadastro/ConectaBanco.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace SistemaCadastro
+{
+    internal class ConectaBanco
+    {
+        MySqlConnection conexao = new MySqlConnection("server=localhost;user id=root;password=;database=banco_cadastro");
+        public String mensagem;
+
+        public DataTable listaGeneros()
+        {
+            MySqlCommand cmd = new MySqlCommand("lista_generos", conexao); // atribui para CMD o comando "lista_generos"
+            cmd.CommandType = CommandType.StoredProcedure; // define o tipo como Stored Procedure
+            try
+            {
+                conexao.Open(); // abre conexão
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd); 
+                DataTable tabela = new DataTable(); // cria uma tabela virtual
+                da.Fill(tabela); // preencher a tabela, a partir do comando cmd
+                return tabela; // retorna a tabela
+            }// fim try
+            catch (MySqlException e)
+            {
+                mensagem = "Erro:" + e.Message;
+                return null;
+            }
+            finally
+            {
+                conexao.Close(); // fecha a conexão
+            }
+
+        }// fim lista_generos 
+    }
+
+}
